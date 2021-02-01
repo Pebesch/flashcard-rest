28f3445a33ba5269c48a580e660cd3bd83a0a5bd
Peter Schmucki <pebs@gmx.ch>
2021-02-01 10:07:36 +0100

    first commit

---
+++ src/main/java/ch/fhnw/webfr/flashcardrest/FlashcardRestApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webfr.flashcardrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/fhnw/webfr/flashcardrest/FlashcardRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webfr.flashcardrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
