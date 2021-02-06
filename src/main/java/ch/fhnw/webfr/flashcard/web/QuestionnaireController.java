ad4ab4af70000f12e8394069f33479f38675ea07
Peter Schmucki <pebs@gmx.ch>
2021-02-06 18:35:09 +0100

    Allowed CrossOrigin on findAll

--- src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ public class QuestionnaireController {
     @Autowired
     private QuestionnaireRepository questionnaireRepository;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<List<Questionnaire>> findAll() {
         return new ResponseEntity<List<Questionnaire>>(questionnaireRepository.findAll(), HttpStatus.OK);
